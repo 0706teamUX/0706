f30ed46e840b6937939a63fd27add587acb1b197
shwogml7 <shwogml21@gmail.com>
2026-07-06 10:48:02 +0900

    practice

---
+++ division.py
@@ -0,0 +1,5 @@
+def division(num1, num2):
+    result = num1/num2
+    return result
+
+division(10, 2)
---
+++ multiplication.py
@@ -0,0 +1,5 @@
+def multiplication(num1, num2):
+    result = num1 * num2
+    return result
+
+multiplication(10, 2)
